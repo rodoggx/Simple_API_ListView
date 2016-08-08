d57cc4b7a2151a060022f4c059b1d3e3f9373d53
rodoggx <mikerodolph@yahoo.com>
2016-08-08 13:32:12 -0400

    first commit

---
+++ app/src/main/java/com/example/simpleapilistview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.simpleapilistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+    String[] myArray = {"Mike", "Tom", "John", "Chris", "Kim", "Sally", "Dave", "Max"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myArray);
+
+        ListView listView = (ListView) findViewById(R.id.myList);
+        listView.setAdapter(adapter);
+
+    }
+}
